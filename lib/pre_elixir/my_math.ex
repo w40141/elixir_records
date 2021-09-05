0dd3fdc7bf86880c11412d0715d8eca35908b14d
w40141 <w.40141@gmail.com>
2021-09-05 14:05:44 +0900

    add collztz module

--- lib/pre_elixir/my_math.ex
+++ lib/pre_elixir/my_math.ex
@@ -11,4 +11,12 @@ defmodule MyMath do
   def permutation(list, n) do
     for x <- list, rest <- permutation(list -- [x], n - 1), do: [x | rest]
   end
+
+  def collatz(n) do
+    [n | _collatz_even(rem(n, 2) == 0, n)]
+  end
+
+  defp _collatz_even(_, 1), do: []
+  defp _collatz_even(true, n), do: collatz(div(n, 2))
+  defp _collatz_even(false, n), do: collatz(3 * n + 1)
 end
